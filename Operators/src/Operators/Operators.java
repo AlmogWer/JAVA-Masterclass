b00c12185699fb99286f97a2ef38b8ce2967d5af
Almog Wertzberger <Almogish@gmail.com>
2021-10-20 15:05:12 +0300

    Ifs

--- Operators/src/Operators/Operators.java
+++ Operators/src/Operators/Operators.java
@@ -42,5 +42,34 @@ public class Operators {
         result-=2;//10-2=8
         System.out.println("10-2= "+result);
 
+        boolean isAlien=false;
+        if(isAlien==false){
+            System.out.println("it is not an alien!");
+            System.out.println("Aliens");
+        }
+
+        int topScore=80;
+        if(topScore<100){
+            System.out.println("You got the high score!");
+        }
+
+        int secondTopScore=85;
+        if((topScore>secondTopScore) && (topScore<100)){
+            System.out.println("Greater than second top score and less than 100");
+        }
+        if((topScore>90)|| (secondTopScore<=90)){
+            System.out.println("Either or both of the condition are true");
+        }
+
+        int newValue=50;
+        if(newValue==50){
+            System.out.println("This is true");
+        }
+
+        boolean isCar=false;
+        if(!isCar){
+            System.out.println("This is not supposed to happen ");
+
+        }
     }
 }
