aa678dcc2c690e72c243301a1c5233332b46e196
Almog Wertzberger <Almogish@gmail.com>
2021-10-20 13:53:46 +0300

    Char and boolean

--- CharAndBoolean/src/CharAndBoolean/CharAndBoolean.java
+++ CharAndBoolean/src/CharAndBoolean/CharAndBoolean.java
@@ -2,6 +2,13 @@ package CharAndBoolean;
 
 public class CharAndBoolean {
     public static void main(String[]args){
+        char myChar='D';
+        char myUnicodeChar='\u00A9';
+        System.out.println(myChar);
+        System.out.println(myUnicodeChar);
+boolean myTrueBooleanValue=true;
+boolean myFalseBooleanValue=false;
+boolean isCustomerOverTwentyOne=true;
 
     }
 }
