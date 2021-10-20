ddf2ac54dab1cb7f3ec9fcb62b52cc3f997b06e0
Almog Wertzberger <Almogish@gmail.com>
2021-10-20 14:31:50 +0300

    Operators

--- Strings/src/Strings/Strings.java
+++ Strings/src/Strings/Strings.java
@@ -14,5 +14,17 @@ public class Strings {
         System.out.println("My string is equal to "+ myString);
         myString=myString+",and this is more.";
         System.out.println("My string is equal to "+ myString);
+        myString=myString+"\u00A9 2021";
+        System.out.println("My string is equal to "+ myString);
+        String numberString="250.55";
+        numberString=numberString+"49.95";
+        System.out.println(numberString);
+        String lastString="10";
+        int myInt=50;
+        lastString=lastString+myInt;
+        System.out.println("LastString is equal to " +lastString);
+        double doubleNumber=120.47d;
+        lastString=lastString+doubleNumber;
+        System.out.println("LastString is equal to " +lastString);
     }
 }
