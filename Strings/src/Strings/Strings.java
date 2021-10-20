c7d9a8300a3c4a8f7025bdfd91cbf6ee29c170f8
Almog Wertzberger <Almogish@gmail.com>
2021-10-20 14:00:52 +0300

    Strings

---
+++ Strings/src/Strings/Strings.java
@@ -0,0 +1,18 @@
+package Strings;
+
+public class Strings {
+    public static void main(String[]args){
+        //byte
+        //short
+        //int
+        //long
+        //float
+        //double
+        //char
+        //boolean
+        String myString="This is a string";
+        System.out.println("My string is equal to "+ myString);
+        myString=myString+",and this is more.";
+        System.out.println("My string is equal to "+ myString);
+    }
+}
