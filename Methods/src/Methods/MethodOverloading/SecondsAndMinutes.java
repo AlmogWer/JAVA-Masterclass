03c857b0b9521695e52921d466ae55c5ee341a77
Almog Wertzberger <Almogish@gmail.com>
2021-10-21 14:57:28 +0300

    Methods

---
+++ Methods/src/Methods/MethodOverloading/SecondsAndMinutes.java
@@ -0,0 +1,28 @@
+package Methods.MethodOverloading;
+
+public class SecondsAndMinutes {
+    public static void main(String[] args) {
+        System.out.println(getDuration(65,45));
+        System.out.println(getDuration(5000));
+    }
+
+    public static String getDuration(int minutes,int seconds){
+        if(minutes<0||seconds<0||seconds>59){
+            System.out.println("Invalid Value");
+        }
+        int hours=minutes/60;
+        int remainingMinutes=minutes%60;
+        return hours+"h "+remainingMinutes+ "m " + seconds+ "s";
+
+    }
+    public static String getDuration(int seconds){
+        if(seconds<=0){
+            return "Invalid Value";
+        }
+        int minutes=seconds/60;
+        int remainingSeconds=seconds%60;
+        return getDuration(minutes,remainingSeconds);
+    }
+
+
+}
