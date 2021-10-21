9a0b13123fa75ca7099e4ee4427eed74e2243d2f
Almog Wertzberger <Almogish@gmail.com>
2021-10-21 11:58:32 +0300

    Methods

---
+++ Methods/src/Methods/LeapYear/Main.java
@@ -0,0 +1,8 @@
+package Methods.LeapYear;
+
+public class Main {
+    public static void main(String[] args) {
+        boolean leapYear=LeapYear.isLeapYear(1600);
+        System.out.println(leapYear);
+    }
+}
