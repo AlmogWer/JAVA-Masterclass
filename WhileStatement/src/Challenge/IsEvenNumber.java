919312806d95327e686af4ae7e44bc25ce4c2383
Almog Wertzberger <Almogish@gmail.com>
2021-10-22 15:51:01 +0300

    While

--- WhileStatement/src/Challenge/IsEvenNumber.java
+++ WhileStatement/src/Challenge/IsEvenNumber.java
@@ -4,12 +4,20 @@ public class IsEvenNumber {
     public static void main(String[] args) {
         int number=4;
         int finishNumber=20;
+        int sum=0;
+        int count=0;
 
         while(number<=finishNumber){
             number++;
             if(!isEvenNumber(number)){
                 continue;
-
+            }
+            sum+=number;
+            count++;
+            if(count==5){
+                System.out.println("even number"+number);
+                System.out.println("Sum= "+sum);
+                break;
             }
             System.out.println("even number"+number);
         }
