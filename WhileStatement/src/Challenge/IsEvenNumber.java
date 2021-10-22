0709f2565af7d52fa5d2e79ab544fe34e6a04e7a
Almog Wertzberger <Almogish@gmail.com>
2021-10-22 13:19:27 +0300

    While

---
+++ WhileStatement/src/Challenge/IsEvenNumber.java
@@ -0,0 +1,14 @@
+package Challenge;
+
+public class IsEvenNumber {
+    public static void main(String[] args) {
+        int number=4;
+
+    }
+    public static boolean isEvenNumber(int number){
+        if(number%2==0){
+            return true;
+        }
+        return false;
+    }
+}
