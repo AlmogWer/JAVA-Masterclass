2349d5d460658da5c90a9b2fd95de0461e64a000
Almog Wertzberger <Almogish@gmail.com>
2021-10-23 20:37:38 +0300

    While

--- WhileStatement/src/Challenge/SharedDigit.java
+++ WhileStatement/src/Challenge/SharedDigit.java
@@ -1,4 +1,11 @@
 package Challenge;
 
 public class SharedDigit {
+    public static boolean hasSharedDigit(int firstNumber,int secondNumber){
+        if(firstNumber<10||firstNumber>99||secondNumber<10||secondNumber>99){
+            return false;
+        }
+        return (firstNumber%10==secondNumber%10)||(firstNumber/10==secondNumber/10)||(firstNumber%10==secondNumber/10)||firstNumber/10==secondNumber%10;
+
+    }
 }
