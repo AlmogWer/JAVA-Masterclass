14a182bbd5b13b8133ad29f1360c4bae123228e8
Almog Wertzberger <Almogish@gmail.com>
2021-10-24 16:16:51 +0300

    Classes

--- Classes/src/Challenges/SimpleCalculator/BankAccount.java
+++ Classes/src/Challenges/SimpleCalculator/BankAccount.java
@@ -1,17 +1,36 @@
 package Challenges.SimpleCalculator;
 
 public class BankAccount {
-    private int accountNumber;
-    private int balance;
+    private String accountNumber;
+    private double balance;
     private String customerName;
     private String email;
     private String phoneNumber;
 
-    public int getAccountNumber() {
+    public BankAccount() {
+        this("56789",2.50,"Default name","Default address","default phone");
+        System.out.println("Empty cons called");
+    }
+
+    public BankAccount(String accountNumber,double balance,String customerName,String email,String phoneNumber){
+        System.out.println("Accont cons with paramaters called");
+        this.accountNumber=accountNumber;
+        this.balance=balance;
+        this.customerName=customerName;
+        this.email=email;
+        this.phoneNumber=phoneNumber;
+    }
+
+    public BankAccount(String customerName, String email, String phoneNumber) {
+        this("999",100.55,customerName,email,phoneNumber);
+
+    }
+
+    public String getAccountNumber() {
         return this.accountNumber;
     }
 
-    public int getBalance() {
+    public double getBalance() {
         return this.balance;
     }
 
@@ -19,46 +38,46 @@ public class BankAccount {
         return this.customerName;
     }
 
-    public String getEmail(){
+    public String getEmail() {
         return this.email;
     }
 
-    public String getPhoneNumber(){
+    public String getPhoneNumber() {
         return this.phoneNumber;
     }
 
-    public void setAccountNumber(int accountNumber){
-        this.accountNumber=accountNumber;
+    public void setAccountNumber(String accountNumber) {
+        this.accountNumber = accountNumber;
     }
 
-    public void setBalance(int balance){
-        this.balance=balance;
+    public void setBalance(int balance) {
+        this.balance = balance;
     }
 
-    public void setCustomerName(String customerName){
-        this.customerName=customerName;
+    public void setCustomerName(String customerName) {
+        this.customerName = customerName;
     }
 
-    public void setEmail(String email){
-        this.email=email;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
-    public void setPhoneNumber(String phoneNumber){
-        this.phoneNumber=phoneNumber;
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
     }
 
-    public void depositFunds(int deposit){
-        this.balance+=deposit;
-        System.out.println("After deposit of "+deposit+", the balance is "+this.balance);
+    public void depositFunds(double deposit) {
+        this.balance += deposit;
+        System.out.println("After deposit of " + deposit + ", the balance is " + this.balance);
     }
 
-    public void withdrawalFunds(int withdrawal){
-        if(this.balance-withdrawal<0){
-            System.out.println("Invalid withdrawal");
-        }else{
+    public void withdrawalFunds(double withdrawal) {
+        if (this.balance - withdrawal < 0) {
+            System.out.println("Invalid withdrawal - only " + this.balance + " is available ");
+        } else {
 
-        this.balance-=withdrawal;
-            System.out.println("After withdrawal of "+withdrawal+", the balance is "+this.balance);
+            this.balance -= withdrawal;
+            System.out.println("After withdrawal of " + withdrawal + ", the balance is " + this.balance);
         }
     }
 
--- Classes/src/Challenges/SimpleCalculator/BankAccountMain.java
+++ Classes/src/Challenges/SimpleCalculator/BankAccountMain.java
@@ -2,11 +2,15 @@ package Challenges.SimpleCalculator;
 
 public class BankAccountMain {
     public static void main(String[] args) {
-        BankAccount Almog= new BankAccount();
+        BankAccount Almog = new BankAccount();//("12345",0.00,"Bob Brown","myemail@bob.com","12345678");
         Almog.depositFunds(300);
         Almog.depositFunds(500);
         Almog.withdrawalFunds(700);
         Almog.withdrawalFunds(150);
+        System.out.println(Almog.getPhoneNumber());
+
+        BankAccount Tims=new BankAccount("Tim","tim@gmail.com","12345");
+        System.out.println(Tims.getPhoneNumber()+" "+Tims.getBalance());
 
     }
 }
