331ea36de605ee929d555f6414f680ca060d3274
Almog Wertzberger <Almogish@gmail.com>
2021-10-24 16:40:05 +0300

    Classes

--- Classes/src/Challenges/SimpleCalculator/VipCustomer.java
+++ Classes/src/Challenges/SimpleCalculator/VipCustomer.java
@@ -2,17 +2,17 @@ package Challenges.SimpleCalculator;
 
 public class VipCustomer {
     private String name;
-    private int creditLimit;
+    private double creditLimit;
     private String email;
 
 
     public VipCustomer(){
         this("Default name",0,"Default@email.com");
     }
-    public VipCustomer(String name,int creditLimit){
+    public VipCustomer(String name,double creditLimit){
         this(name,creditLimit,"default@gmail.com");
     }
-    public VipCustomer(String name,int creditLimit,String email){
+    public VipCustomer(String name,double creditLimit,String email){
         this.name=name;
         this.creditLimit=creditLimit;
         this.email=email;
@@ -21,7 +21,7 @@ public class VipCustomer {
         return name;
     }
 
-    public int getCreditLimit() {
+    public double getCreditLimit() {
         return creditLimit;
     }
 
