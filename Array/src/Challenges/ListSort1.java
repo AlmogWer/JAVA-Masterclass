fc7911198803f637f9c9d5dd408792df02529016
Almog Wertzberger <Almogish@gmail.com>
2021-10-25 18:46:17 +0300

    Arrays

---
+++ Array/src/Challenges/ListSort1.java
@@ -0,0 +1,12 @@
+package Challenges;
+
+import java.util.Scanner;
+
+public class ListSort1 {
+    public Scanner scanner=new Scanner(System.in);
+    public static void main(String[] args) {
+
+    }
+    public static int getIntegers()
+
+}
