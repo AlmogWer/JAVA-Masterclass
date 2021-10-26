f041c5bee0118947845e7029a81d040ad56269c2
Almog Wertzberger <Almogish@gmail.com>
2021-10-26 12:13:54 +0300

    Arrays

--- Array/src/RefVsValues.java
+++ Array/src/RefVsValues.java
@@ -2,34 +2,36 @@ import java.util.Arrays;
 
 public class RefVsValues {
     public static void main(String[] args) {
-        int myIntValue=10;
-        int anotherIntValue=myIntValue;
-        System.out.println("myIntValue= "+myIntValue);
-        System.out.println("anotherIntValue= "+anotherIntValue);
+        int myIntValue = 10;
+        int anotherIntValue = myIntValue;
+        System.out.println("myIntValue= " + myIntValue);
+        System.out.println("anotherIntValue= " + anotherIntValue);
 
         anotherIntValue++;
-        System.out.println("myIntValue= "+myIntValue);
-        System.out.println("anotherIntValue= "+anotherIntValue);
+        System.out.println("myIntValue= " + myIntValue);
+        System.out.println("anotherIntValue= " + anotherIntValue);
 
-        int[] myIntArray=new int[5];
+        int[] myIntArray = new int[5];
 
-        int[] anotherArray=myIntArray;
-        System.out.println("MyIntArray "+ Arrays.toString(myIntArray));
-        System.out.println("anotherArray "+ Arrays.toString(anotherArray));
+        int[] anotherArray = myIntArray;
+        System.out.println("MyIntArray " + Arrays.toString(myIntArray));
+        System.out.println("anotherArray " + Arrays.toString(anotherArray));
 
-        anotherArray[0]=1;
+        anotherArray[0] = 1;
 
-        System.out.println("after change MyIntArray "+ Arrays.toString(myIntArray));
-        System.out.println("after change anotherArray "+ Arrays.toString(anotherArray));
+        System.out.println("after change MyIntArray " + Arrays.toString(myIntArray));
+        System.out.println("after change anotherArray " + Arrays.toString(anotherArray));
 
-modifyArray(myIntArray);
+        anotherArray = new int[]{4, 5, 6, 7, 8};
+        modifyArray(myIntArray);
 
-        System.out.println("after modify MyIntArray "+ Arrays.toString(myIntArray));
-        System.out.println("after modify anotherArray "+ Arrays.toString(anotherArray));
+        System.out.println("after modify MyIntArray " + Arrays.toString(myIntArray));
+        System.out.println("after modify anotherArray " + Arrays.toString(anotherArray));
     }
 
-    private static void modifyArray(int[] array){
+    private static void modifyArray(int[] array) {
 
-        array[0]=2;
+        array[0] = 2;
+        array = new int[]{1, 2, 3, 4, 5};
     }
 }
