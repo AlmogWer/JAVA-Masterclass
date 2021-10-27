d6ae89aaa187edfe7868a0b9f2f3f99c5777bb4d
Almog Wertzberger <Almogish@gmail.com>
2021-10-27 14:16:31 +0300

    Lists

---
+++ Array/src/List/MobilePhone/Main.java
@@ -0,0 +1,71 @@
+package List.MobilePhone;
+
+import java.util.Scanner;
+
+public class Main {
+    private static Scanner scanner = new Scanner(System.in);
+    private static MobilePhone mobilePhone = new MobilePhone("0039 330 4404");
+
+    public static void main(String[] args) {
+        boolean quit = false;
+        startPhone();
+        printActions();
+        while (!quit) {
+            System.out.println("\nEnter action: (6 to show availble actions)");
+            int action=scanner.nextInt();
+            scanner.nextLine();
+            switch (action){
+                case 0:
+                    System.out.println("\nSutting down...");
+                    quit=true;
+                    break;
+                case 1:
+                    mobilePhone.printContacts();
+                    break;
+                case 2:
+                    addNewContact();
+                    break;
+                case 3:
+                    updateContact();
+                    break;
+                case 4:
+                    removeContact();
+                    break;
+                case 5:
+                    queryContact();
+                    break;
+                case 6:
+                    printActions();
+                    break;
+            }
+
+        }
+    }
+
+    private static void addNewContact(){
+        System.out.println("Enter new contact name: ");
+        String name=scanner.nextLine();
+        System.out.println("Enter phone number: ");
+        String phone=scanner.nextLine();
+        Contact newContact=Contact.createContact(name,phone);
+    }
+
+    private static void startPhone() {
+        System.out.println("Starting phone...");
+    }
+
+    private static void printActions() {
+        System.out.println("\nAvailble actions: \npress");
+        System.out.println("0 - to shutdown\n" +
+                "1 - to print contacts\n" +
+                "2 - to add a new contact\n" +
+                "3 - to update existing an existing contact\n" +
+                "4 - to remove an existing contact \n" +
+                "5 - query if an existing contact exists\n" +
+                "6 - to print a list of a availble actions.");
+        System.out.println("Choose your actions");
+
+
+    }
+
+}
