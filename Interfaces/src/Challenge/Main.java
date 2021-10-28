f48369f2ccb71f5712ddfbc8937d5fdab33a3402
Almog Wertzberger <Almogish@gmail.com>
2021-10-28 15:37:14 +0300

    Interfaces

---
+++ Interfaces/src/Challenge/Main.java
@@ -0,0 +1,153 @@
+package Challenge;
+
+import java.util.*;
+
+public class Main {
+    private static List<Album> albums = new ArrayList<Album>();
+
+    public static void main(String[] args) {
+        Album album = new Album("Stormbringer", "Deep Purple");
+        album.addSong("StormBringer", 4.6);
+        album.addSong("holy man", 4.22);
+        album.addSong("hold on", 4.3);
+        album.addSong("double dealer", 5.6);
+        album.addSong("Cant do it right", 3.21);
+        album.addSong("Ball shooter", 6.23);
+        album.addSong("The gypsy", 4.27);
+        album.addSong("Just song", 3.43);
+        album.addSong("Soldier of fortune", 3.13);
+
+        albums.add(album);
+
+        album = new Album("For those about to rock", "AC/DC");
+        album.addSong("For those about to rock", 5.44);
+        album.addSong("Finger", 3.45);
+        album.addSong("lets go", 4.44);
+        album.addSong("Venom", 2.34);
+        album.addSong("COD", 6.47);
+        album.addSong("Break", 5.54);
+        album.addSong("The", 3.16);
+        album.addSong("Rules", 4.423);
+        albums.add(album);
+
+        List<Song> playList = new ArrayList<>();
+        albums.get(0).addToPlayList("Cant do it right", playList);
+        albums.get(0).addToPlayList("StormBringer", playList);
+        albums.get(0).addToPlayList("shooter", playList); // doesn't exist validation
+        albums.get(0).addToPlayList(9, playList);
+        albums.get(1).addToPlayList(8, playList);
+        albums.get(1).addToPlayList(3, playList);
+        albums.get(1).addToPlayList(2, playList);
+        albums.get(1).addToPlayList(22, playList);// doesn't exist validation
+
+        play(playList);
+
+    }
+
+    public static void play(List<Song> playList) {
+        Scanner scanner = new Scanner(System.in);
+        boolean quit = false;
+        boolean forward = true;
+        ListIterator<Song> listIterator = playList.listIterator();
+        if (playList.size() == 0) {
+            System.out.println("No songs in playlist");
+            return;
+        } else {
+            System.out.println("Now playing " + listIterator.next().toString());
+            printMenu();
+        }
+
+        while (!quit) {
+            int action = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (action) {
+                case 0:
+                    System.out.println("Playlist complete.");
+                    quit = true;
+                    break;
+                case 1:
+                    if (!forward) {
+                        if (listIterator.hasNext()) {
+                            listIterator.next();
+                        }
+                        forward = true;
+                    }
+                    if (listIterator.hasNext()) {
+                        System.out.println("Now playing " + listIterator.next().toString());
+                    } else
+                        System.out.println("Reach the end of the playlist");
+                    forward = false;
+                    break;
+                case 2:
+                    if (forward) {
+                        if (listIterator.hasPrevious()) {
+                            listIterator.previous();
+                        }
+                        forward = false;
+                    }
+                    if (listIterator.hasPrevious()) {
+                        System.out.println("Now playing " + listIterator.previous().toString());
+                    } else {
+                        System.out.println("Start of the playing");
+                        forward = true;
+                    }
+                    break;
+                case 3:
+                    if (forward) {
+                        if (listIterator.hasPrevious()) {
+                            System.out.println("Now replaying " + listIterator.previous().toString());
+                            forward = false;
+                        } else {
+                            System.out.println("Were at the start of the list");
+                        }
+                    } else {
+                        if (listIterator.hasNext()) {
+
+                            System.out.println("Now replaying " + listIterator.next().toString());
+                            forward = true;
+                        } else {
+                            System.out.println("We have reached the end of the list");
+                        }
+                    }
+                    break;
+                case 4:
+                    printList(playList);
+                    break;
+                case 5:
+                    printMenu();
+                    break;
+                case 6:
+                    if (playList.size() > 0) {
+                        listIterator.remove();
+                        if (listIterator.hasNext()) {
+                            System.out.println("Now playing " + listIterator.next());
+                        } else if (listIterator.hasPrevious()) {
+                            System.out.println("Now playing " + listIterator.previous());
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+
+    private static void printMenu() {
+        System.out.println("Availble actions: \npress");
+        System.out.println("0 - to quit\n" +
+                "1 - to play next song \n" +
+                "2 - to play previous song\n" +
+                "3 - to replay the current song\n" +
+                "4 - list songs in the playlist\n" +
+                "5 - print available actions\n" +
+                "6 - delete current song from playlist");
+    }
+
+    public static void printList(List<Song> playList) {
+        Iterator<Song> iterator = playList.iterator();
+        System.out.println("===============");
+        while (iterator.hasNext()) {
+            System.out.println(iterator.next());
+        }
+        System.out.println("===============");
+    }
+}
