cb5c482f5942e270baeb5b622ff37c4c7241fc20
Almog Wertzberger <87434394+AlmogWer@users.noreply.github.com>
2021-12-16 15:37:20 +0200

    16.12

--- DebuggerDemo/src/Banking/BankAccount.java
+++ DebuggerDemo/src/Banking/BankAccount.java
@@ -1,14 +1,18 @@
 package Banking;
 
 public class BankAccount {
+    public static final int CHECKING = 1;
+    public static final int SAVINGS = 2;
     private String firstName;
     private String lastName;
     private double balance;
+    private int accountType;
 
-    public BankAccount(String firstName, String lastName, double balance) {
+    public BankAccount(String firstName, String lastName, double balance, int typeOfAccount) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.balance = balance;
+        this.accountType = typeOfAccount;
     }
 
     //the branch arguement is true if the cosumter is performing transaction
@@ -27,6 +31,10 @@ public class BankAccount {
 
     public double getBalance() {
         return balance;
+    }
+
+    public boolean isChecking() {
+        return accountType == CHECKING;
     }
 
     //more methods that use firstname lastname and perform other functions
--- DebuggerDemo/src/Banking/BankAccountTest.java
+++ DebuggerDemo/src/Banking/BankAccountTest.java
@@ -1,12 +1,15 @@
 package Banking;
 
-import static org.junit.Assert.fail;
+import static org.junit.Assert.*;
 
 public class BankAccountTest {
 
     @org.junit.Test
     public void deposit() throws Exception {
-        fail("This tetst has yet to be implemented");
+        BankAccount account = new BankAccount("Almog", "Wer", 1000.00, BankAccount.CHECKING);
+        double balance = account.deposit(200.00, true);
+        assertEquals(1200.00, balance, 0);
+
     }
 
     @org.junit.Test
@@ -15,8 +18,25 @@ public class BankAccountTest {
     }
 
     @org.junit.Test
-    public void getBalance() throws Exception {
-        fail("This tetst has yet to be implemented");
+    public void getBalance_deposit() throws Exception {
+        BankAccount account = new BankAccount("Almog", "Wer", 1000.00, BankAccount.CHECKING);
+        account.deposit(200.00, true);
+        assertEquals(1200.00, account.getBalance(), 0);
     }
+
+    @org.junit.Test
+    public void getBalance_withraw() throws Exception {
+        BankAccount account = new BankAccount("Almog", "Wer", 1000.00, BankAccount.CHECKING);
+        account.withdraw(200.00, true);
+        assertEquals(800.00, account.getBalance(), 0);
+    }
+
+    @org.junit.Test
+    public void isChecking_true() {
+        BankAccount account = new BankAccount("Almog", "Wer", 1000.00, BankAccount.CHECKING);
+        assertTrue(account.isChecking());
+
+    }
+
 
 }
