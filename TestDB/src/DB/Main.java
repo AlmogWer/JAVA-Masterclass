29de4ba639c0832655dfbcfffba8ed3e762ab1f8
Almog Wertzberger <87434394+AlmogWer@users.noreply.github.com>
2021-12-19 16:17:45 +0200

    19.12

--- TestDB/src/DB/Main.java
+++ TestDB/src/DB/Main.java
@@ -1,9 +1,6 @@
 package DB;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-import java.sql.Statement;
+import java.sql.*;
 
 public class Main {
 
@@ -14,8 +11,24 @@ public class Main {
 
         try {
             Connection conn = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\almogw\\IdeaProjects\\JAVA-Masterclass\\TestDB\\tstjava.db");
+//            conn.setAutoCommit(false);
             Statement statement = conn.createStatement();
-            statement.execute("create table contacts (name TEXT ,phone INTEGER, email TEXT)");
+            statement.execute("create table if not exists contacts" + " (name TEXT ,phone INTEGER, email TEXT)");
+
+//            statement.execute("insert into contacts (name,phone,email) " + "values ('Joe',45632,'joe@gmail.com')");
+//            statement.execute("insert into contacts (name,phone,email) " + "values ('Jane',4829484,'Jane@somewhere.com')");
+//            statement.execute("insert into contacts (name,phone,email) " + "values ('Fido',9038,'dog@email.com')");
+//            statement.execute("UPDATE contacts set phone=5566789 where name='Jane'");
+//            statement.execute("delete from contacts where name='Joe'");
+
+//            statement.execute("select * from contacts ");
+//            ResultSet results = statement.getResultSet();
+            ResultSet results = statement.executeQuery("Select * from contacts");
+            while (results.next()) {
+                System.out.println(results.getString("name") + " " + results.getInt("phone") + " " + results.getString("email"));
+            }
+
+            results.close();
 
             statement.close();
             conn.close();
