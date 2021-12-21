69528dd88f745b72151d2a80ff053e072ac9d4cc
Almog Wertzberger <87434394+AlmogWer@users.noreply.github.com>
2021-12-21 15:36:24 +0200

    21.12

--- UDP/src/UDPClient/Main.java
+++ UDP/src/UDPClient/Main.java
@@ -10,7 +10,7 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         try {
-            InetAddress address = InetAddress.getLocalHost();
+            InetAddress address = InetAddress.getLocalHost(); //getByName()
             DatagramSocket datagramSocket = new DatagramSocket();
             Scanner scanner = new Scanner(System.in);
             String echoString;
@@ -21,6 +21,11 @@ public class Main {
                 byte[] buffer = echoString.getBytes();
                 DatagramPacket packet = new DatagramPacket(buffer, buffer.length, address, 5000);
                 datagramSocket.send(packet);
+
+                byte[] buffer2 = new byte[50];
+                packet = new DatagramPacket(buffer2, buffer2.length);
+                datagramSocket.receive(packet);
+                System.out.println("Text received is " + new String(buffer2, 0, packet.getLength()));
 
             } while (!echoString.equals("exit"));
 
